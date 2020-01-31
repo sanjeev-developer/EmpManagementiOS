5991581b36ea17dec7e4b08131b57fd8c828dfe2
sanjeev <sanjeevgupta9223@gmail.com>
2020-01-30 19:53:10 -0500

    CoreData commit

--- Vehdata+CoreDataProperties.swift
+++ Vehdata+CoreDataProperties.swift
@@ -11,18 +11,4 @@ import Foundation
 import CoreData
 
 
-extension Vehdata {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<Vehdata> {
-        return NSFetchRequest<Vehdata>(entityName: "Vehdata")
-    }
-
-    @NSManaged public var company: String?
-    @NSManaged public var empid: String?
-    @NSManaged public var model: String?
-    @NSManaged public var plate: String?
-    @NSManaged public var vehicletype: String?
-    @NSManaged public var year: String?
-    @NSManaged public var emp: Empdata?
-
-}
